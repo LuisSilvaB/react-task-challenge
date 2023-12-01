34aaa4fa4744e24635d2d2d56518c606a621faad
e <LuisSilvaBalladares@hotmail.com>
2023-12-01 12:27:23 -0500

    feat: add formTask styles

--- src/components/formTask/formTask.jsx
+++ src/components/formTask/formTask.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
+import { POINT_ESTIMATE } from '../../utils/constants.js'
 
 import './desktop-formTask.css'
 import './android-formTask.css'
 import './iphone-formTask.css'
 
+import formEstimate from '/img/taskForm/formEstimate.svg'
+import ravnLogo from '/img/sidebar/ravnLogo.svg'
+
 export default function FormTask(props) {
     const {typeDevice, typeForm, createTask, isVisible, setIsViseble} = props;
   return (
     <div className={`${typeDevice}-formTask`}>
-        formTask
+      <div className={`${typeDevice}-formTask__shadow`} />
+      <div className={`${typeDevice}-formtask__content`}> 
+        <input type="text" placeholder='Task Title' className={`${typeDevice}-formtask__input ${typeDevice}-regular-xL-body`}/>
+        <div className={`${typeDevice}-formtask__options-container`}>
+          <select name="" id="" className={`${typeDevice}-form__select`}>
+
+            <option value="" className={`${typeDevice}-form__option`} defaultChecked disabled>ESTIMATE</option>
+            {
+              Object.keys(POINT_ESTIMATE).map((el, key) => (
+                  
+                  <option key={key} value={el} className={`${typeDevice}-form__option`}>
+                    <img src={ravnLogo} alt=""/>
+                    {(POINT_ESTIMATE[el])}
+                    </option>
+                )
+              )
+            }
+          </select>
+        </div>
+      </div>
     </div>
   )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,9 @@
 export const TASK_TAG =['ANDROID', 'IOS', 'NODE_JS', 'RAILS', 'REACT']
 export const STATUS = [ 'BACKLOG', 'CANCELLED', 'DONE', 'IN_PROGRESS', 'TODO']
-export const POINT_ESTIMATE = ['EIGHT', 'FOUR', 'ONE', 'TWO', 'ZERO']
+export const POINT_ESTIMATE = {
+    ZERO:0,
+    ONE:1,
+    TWO:2,
+    FOUR:4,
+    EIGHT:8,
+}
